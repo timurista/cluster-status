9cab439fb550982a8bb36753a1596a99c9b790a0
Tim Urista <timothy.urista@gmail.com>
2020-11-17 09:56:36 -0800

    add links for the output of status page

--- status/main.go
+++ status/main.go
@@ -5,7 +5,14 @@ import (
 	"html/template"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 // Site status
 const (
@@ -14,13 +21,15 @@ const (
 )
 
 type StatusPageData struct {
-	PageTitle string
-	links     []string
+	PageTitle         string
+	Links             []string
+	WebsocketLocation string
 }
 
 func main() {
 
-	tmpl := template.Must(template.ParseFiles("layout.html"))
+	tmpl := template.Must(template.ParseFiles("pages/layout.html"))
+	port := "8081"
 	links := []string{
 		"http://google.com",
 		"http://facebook.com",
@@ -30,43 +39,108 @@ func main() {
 		"http://twitter.com",
 	}
 
-	c := make(chan string)
+	// c := make(chan string)
 
-	for _, link := range links {
-		go checkLink(link, c, tmpl)
-	}
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
 
-	for l := range c {
-		go func(link string) {
+		for {
 			time.Sleep(time.Second * 5)
-			checkLink(link, c, tmpl)
-		}(l)
-	}
+
+			c := make(chan string)
+
+			for _, link := range links {
+				go checkLink(link, c, conn)
+			}
+
+			for l := range c {
+				go func(link string) {
+					time.Sleep(time.Second * 5)
+					checkLink(link, c, conn)
+				}(l)
+			}
+
+		}
+	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("links")
+		fmt.Println(links)
+		// linkStrings := []string{}
+		// for _, link := range links {
+		// 	_, err := http.Get(link)
+		// 	status := ""
+		// 	if err != nil {
+		// 		status = "X"
+		// 	} else {
+		// 		status = "UP"
+		// 	}
+		// 	linkStrings = append(linkStrings, status+" "+link)
+		// }
+
 		data := StatusPageData{
-			PageTitle: "Cluster Status Page",
-			links:     links,
+			PageTitle:         "Cluster Status Page",
+			Links:             links,
+			WebsocketLocation: "localhost:" + port,
 		}
 
 		tmpl.Execute(w, data)
+
 	})
-	http.ListenAndServe(":8081", nil)
+	http.ListenAndServe(":"+port, nil)
+	fmt.Println("listening at " + port)
 }
 
-func checkLink(link string, c chan string, tmpl *template.Template) {
+func checkLink(link string, c chan string, conn *websocket.Conn) {
 	_, err := http.Get(link)
 	// if err != nil {
 	// 	return DOWN
 	// }
 	// return UP
 
+	// msgType := websocket.TextMessage
+
 	if err != nil {
-		fmt.Println(link + " might be down")
+		var v struct {
+			link   string
+			status string
+		}
+		// tmpl.Execute(w, data)
+		v.link = link
+		v.status = "down"
+		// Partial JSON values.
+
+		// data, _ := json.Marshal(v)
+
+		msg := link + " might be down"
+		if err := conn.WriteJSON(&v); err != nil {
+			return
+		}
+		fmt.Println(msg)
 		// tmpl.Execute(w, data)
 		c <- link
 	} else {
-		fmt.Println(link + " is up")
+		type v struct {
+			Link        string `json:"link"`
+			Status      string `json:"status"`
+			LastUpdated string `json:"lastUpdated"`
+		}
+		// v.link = link
+		// v.status = "up"
+		// Partial JSON values.
+		m := v{
+			Link:        link,
+			Status:      "up",
+			LastUpdated: time.Now().Local().String(),
+		}
+		// data, _ := json.Marshal(v)
+
+		// msg := link + "is up"
+		if err := conn.WriteJSON(m); err != nil {
+			return
+		}
+		fmt.Println(m)
 		c <- link
 	}
 }
